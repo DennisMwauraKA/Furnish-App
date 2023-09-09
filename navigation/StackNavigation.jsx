5db8a4c1d680d91af9c0950e35c94da17c6db65c
DennisMwauraKA <mwaurakariuki70@gmail.com>
2023-09-09 14:45:56 +0300

    third commit

--- navigation/StackNavigation.jsx
+++ navigation/StackNavigation.jsx
@@ -1,10 +1,10 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "@expo/vector-icons/Ionicons";
-
+import { useCart } from "../cart/CartContext";
 //importing the screens
 import HomeScreen from "../screens/HomeScreen";
 import SearchScreen from "../screens/SearchScreen";
@@ -12,17 +12,21 @@ import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import CartScreen from "../screens/CartScreen";
 import ProductScreen from "../screens/ProductScreen";
+import ConfirmScreen from "../screens/ConfirmScreen";
 //create the stack containers to hold the screens
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const BottomTab = ({ route }) => {
+  const { cartState } = useCart();
+  const { cartItems } = cartState;
   return (
     <Tab.Navigator
       initialRouteName="Home"
       screenOptions={({ route }) => ({
         tabBarActiveTintColor: "orange",
         tabBarInactiveTintColor: "grey",
+        tabBarHideOnKeyboard: true,
         tabBarLabelStyle: {
           paddingBottom: 10,
           fontSize: 10,
@@ -57,7 +61,33 @@ const BottomTab = ({ route }) => {
       <Tab.Screen
         name="Cart"
         component={CartScreen}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <View style={{ position: "relative" }}>
+              <Ionicons name="cart" size={size} color={color} />
+              {cartItems.length > 0 && (
+                <View
+                  style={{
+                    position: "absolute",
+                    width: 20,
+                    height: 20,
+                    alignItems: "center",
+                    backgroundColor: "red",
+                    borderRadius: 10,
+                    padding: 3,
+                    top: -6,
+                    right: -15,
+                  }}
+                >
+                  <Text style={{ color: "white", fontSize: 12 }}>
+                    {cartItems.length}
+                  </Text>
+                </View>
+              )}
+            </View>
+          ),
+        }}
       />
     </Tab.Navigator>
   );
@@ -83,12 +113,15 @@ const StackNavigation = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen
+          name="Confirmation"
+          component={ConfirmScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
           name="Bottom"
           component={BottomTab}
           options={{ headerShown: false }}
         />
-        
-      
       </Stack.Navigator>
     </NavigationContainer>
   );
